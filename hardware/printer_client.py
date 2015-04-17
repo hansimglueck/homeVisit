58ad40f6f1fba27ee5bd35ad01cd32780456096d
h*ns <hans@dizzi.biz>
2015-04-17 22:27:58 +0200

    closes #6

--- hardware/printer_client.py
+++ hardware/printer_client.py
@@ -34,14 +34,20 @@ def cb(msg):
 		#p.print_text("\x84")
 		p.double_width(True)
 		p.set_linespacing(35)
+		char_count = 0
 		for item in lines:
 			#print item
 			item = replaceSpecialChars(item)
 			unwrapped = item
 			wrapped = textwrap.fill(unwrapped, 16)
+			char_count = char_count + len(wrapped)
 			#print "printing: "+wrapped
 			p.print_text(wrapped)
 			p.print_text("\n")
+			if (char_count > 250):
+				time.sleep(1.5)
+				print(char_count)
+				char_count = 0
 		#unwrapped = txt
 		#wrapped = textwrap.fill(unwrapped, 16)
 		#p.print_text(wrapped)
