0ec24f4aef7cf0ff0d249aa4ecccd3215c746a9a
gritschu <grit@nuuskamunensmbp.visit.eu>
2015-04-18 14:27:43 +0200

    MC: neues Layout in players, dimm heller

--- mc/js/controllers/players.js
+++ mc/js/controllers/players.js
@@ -19,15 +19,15 @@ angular.module('homeVisitMCApp')
         $scope.tableDisplay = {
             type: 'playback'
         };
-        $scope.players = [
-            {playerId: 0, name: 'player1', top: 10, left: 189, color: '#000000'},
-            {playerId: 1, name: 'player2', top: 10, left: 377, color: '#000000'},
-            {playerId: 2, name: 'player3', top: 10, left: 565, color: '#000000'},
-            {playerId: 3, name: 'player4', top: 10, left: 753, color: '#000000'},
-            {playerId: 4, name: 'player5', top: 10, left: 941, color: '#000000'},
-            {playerId: 5, name: 'player6', top: 150, left: 1110, color: '#000000'},
-            {playerId: 6, name: 'player7', top: 340, left: 1110, color: '#000000'},
-            {playerId: 7, name: 'player8', top: 490, left: 941, color: '#000000'}
+        $scope.playerPos = [
+            {top: 10, left: 15},
+            {top: 10, left: 330},
+            {top: 10, left: 645},
+            {top: 10, left: 960},
+            {top: 490, left: 960},
+            {top: 490, left: 645},
+            {top: 490, left: 330},
+            {top: 490, left: 15}
         ];
     })
     .controller("PlayerDetailsCtrl", function ($scope) {
