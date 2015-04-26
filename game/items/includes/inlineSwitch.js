4509a71b24d0b89c368ca7f327ff1619a978e20f
h*ns <hans@dizzi.biz>
2015-04-26 18:49:30 +0200

    inline-switch soll "source"-type bekommen

--- game/items/includes/inlineSwitch.js
+++ game/items/includes/inlineSwitch.js
@@ -1,18 +1,23 @@
 module.exports = {
     executeItem: function () {
-        this.log("looking for deck for option " + this.param);
+        var option = this.param;
+        if (this.inlineSwitchSource === 'previousStep') {
+            var data = this.previous.getData();
+            option = data.complete ? data.voteOptions[0].value : -1;
+        }
+        this.log("looking for deck for option " + option);
         if (typeof this.inlineDecks === "undefined") return;
-        var deck = this.inlineDecks[this.param];
+        var deck = this.inlineDecks[option];
         if (typeof deck == "undefined") {
             this.log("no matching option found", true);
             this.step();
         }
         else {
-            this.log("inserting deck for option " + this.param, true);
+            this.log("inserting deck for option " + option, true);
             var oldNext = this.next;
             this.next = null;
             for (var i = 0; i < deck.items.length; i++) {
-                this.appendItem(new SequenceItem(null, deck.items[i], this.index + ":" + this.param + ":" + i, true));
+                this.appendItem(new SequenceItem(null, deck.items[i], this.index + ":" + option + ":" + i, true));
             }
             this.appendItem(oldNext);
             this.step();
