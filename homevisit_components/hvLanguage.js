a464ec773a473608fbd4395eca2322f16fea3844
Mirko Dietrich <mirko.dietrich@l4m1.de>
2015-04-30 19:37:37 +0200

    I18n: import script extracts translations now

--- homevisit_components/hvLanguage.js
+++ homevisit_components/hvLanguage.js
@@ -2,16 +2,16 @@ angular.module('hvLanguage', [])
     .factory('languageFactory', ['gettext', function(gettext) {
 
         var languages = [
-                ['en', gettext('English')],
-                ['de', gettext('German')],
-                ['cs', gettext('Czech')],
-                ['da', gettext('Danish')],
-                ['fr', gettext('French')],
-                ['nl', gettext('Dutch')],
-                ['no', gettext('Norwegian')],
-                ['pl', gettext('Polish')],
-                ['pt', gettext('Portuguese')]
-            ]
+            ['en', gettext('English')],
+            ['de', gettext('German')],
+            ['cs', gettext('Czech')],
+            ['da', gettext('Danish')],
+            ['fr', gettext('French')],
+            ['nl', gettext('Dutch')],
+            ['no', gettext('Norwegian')],
+            ['pl', gettext('Polish')],
+            ['pt', gettext('Portuguese')]
+        ];
 
         var languageFactory = {};
 
