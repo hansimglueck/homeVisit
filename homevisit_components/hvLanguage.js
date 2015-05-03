04c4d234c867ac9dfd745ee31bc63c379dd81c9d
Mirko Dietrich <mirko.dietrich@l4m1.de>
2015-05-04 00:51:34 +0200

    jshinted homevisit_components/hvLanguage.js

--- homevisit_components/hvLanguage.js
+++ homevisit_components/hvLanguage.js
@@ -1,36 +1,41 @@
-angular.module('hvLanguage', [])
-    .factory('languageFactory', function(gettextCatalog) {
+(function() {
+    'use strict';
 
-        var languages = [
-            ['en', gettextCatalog.getString('English')],
-            ['de', gettextCatalog.getString('German')],
-            ['cs', gettextCatalog.getString('Czech')],
-            ['da', gettextCatalog.getString('Danish')],
-            ['fr', gettextCatalog.getString('French')],
-            ['nl', gettextCatalog.getString('Dutch')],
-            ['no', gettextCatalog.getString('Norwegian')],
-            ['pl', gettextCatalog.getString('Polish')],
-            ['pt', gettextCatalog.getString('Portuguese')]
-        ];
+    angular.module('hvLanguage', [])
+        .factory('languageFactory', function(gettextCatalog) {
 
-        var languageFactory = {};
+            var languages = [
+                ['en', gettextCatalog.getString('English')],
+                ['de', gettextCatalog.getString('German')],
+                ['cs', gettextCatalog.getString('Czech')],
+                ['da', gettextCatalog.getString('Danish')],
+                ['fr', gettextCatalog.getString('French')],
+                ['nl', gettextCatalog.getString('Dutch')],
+                ['no', gettextCatalog.getString('Norwegian')],
+                ['pl', gettextCatalog.getString('Polish')],
+                ['pt', gettextCatalog.getString('Portuguese')]
+            ];
 
-        languageFactory.availableLanguages = languages;
+            var languageFactory = {};
 
-        return languageFactory;
-    })
-    .directive('languageChooser', function() {
-        return {
-            restrict: 'E',
-            replace: 'true',
-            templateUrl: '/homevisit_components/views/language-chooser.html',
-            controller: function ($scope, Socket, languageFactory, gettextCatalog) {
-                $scope.gettextCatalog = gettextCatalog;
-                $scope.languages = languageFactory.availableLanguages;
-                $scope.changeLanguage = function() {
-                    Socket.emit('changeLanguage', gettextCatalog.getCurrentLanguage());
-                };
-            },
-            scope: {}
-        };
-    });
+            languageFactory.availableLanguages = languages;
+
+            return languageFactory;
+        })
+        .directive('languageChooser', function() {
+            return {
+                restrict: 'E',
+                replace: 'true',
+                templateUrl: '/homevisit_components/views/language-chooser.html',
+                controller: function ($scope, Socket, languageFactory, gettextCatalog) {
+                    $scope.gettextCatalog = gettextCatalog;
+                    $scope.languages = languageFactory.availableLanguages;
+                    $scope.changeLanguage = function() {
+                        Socket.emit('changeLanguage', gettextCatalog.getCurrentLanguage());
+                    };
+                },
+                scope: {}
+            };
+        });
+
+})();
