39c66f968c822ee7ec33cbd07ba6fa03db030240
Mirko Dietrich <mirko.dietrich@l4m1.de>
2015-05-03 23:05:17 +0200

    jshinted admin/js/services/gameServices.js

--- admin/js/services/gameServices.js
+++ admin/js/services/gameServices.js
@@ -1,28 +1,29 @@
-/**
- * Created by jeanbluer on 16.01.15.
- */
+(function() {
+    'use strict';
 
-var gameServices = angular.module('gameServices', ['ngResource']);
+    var gameServices = angular.module('gameServices', ['ngResource']);
 
-gameServices.factory('Game', ['$resource', function ($resource) {
-    return $resource('/decks/:id', null, {
-        'update': {method: 'PUT'}
-    });
-    //return $resource('/sequenceItems', {}, {
-    //    query: {method:'GET', params:{}, isArray:true}
-    //});
-}])
-    .factory('GameConf', ['$resource', function ($resource) {
-        return $resource('/gameConf/:id', null, {
-            'update': {
-                method: 'PUT',
-                interceptor: {
-                    responseError: function (data) {
-                        return "ERROR";
+    gameServices
+        .factory('Game', function ($resource) {
+            return $resource('/decks/:id', null, {
+                'update': {method: 'PUT'}
+            });
+            //return $resource('/sequenceItems', {}, {
+            //    query: {method:'GET', params:{}, isArray:true}
+            //});
+        })
+        .factory('GameConf', ['$resource', function ($resource) {
+            return $resource('/gameConf/:id', null, {
+                'update': {
+                    method: 'PUT',
+                    interceptor: {
+                        responseError: function (data) {
+                            return "ERROR";
+                        }
                     }
-                }
-            },
-            'getRun': {method: 'GET', url: '/gameConf/run'}
-        });
-    }])
-;
+                },
+                'getRun': {method: 'GET', url: '/gameConf/run'}
+            });
+        }]);
+
+})();
