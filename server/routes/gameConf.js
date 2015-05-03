fe344f37cf9bf3ff6645f725e0bf9741c2259dda
Mirko Dietrich <mirko.dietrich@l4m1.de>
2015-05-03 19:50:41 +0200

    jshinted server/routes/gameConf.js

--- server/routes/gameConf.js
+++ server/routes/gameConf.js
@@ -1,52 +1,63 @@
-var express = require('express');
-var router = express.Router();
-var mongoConnection = require('../../homevisit_components/mongo/mongoConnection.js');
-var game = require('../../game/game.js');
-var gameConf = require('../../game/gameConf.js');
-var ObjectID = require('mongodb').ObjectID;
+(function() {
+    'use strict';
 
-/* GET /gameConf listing. */
+    var express = require('express');
+    var router = express.Router();
+    var mongoConnection = require('../../homevisit_components/mongo/mongoConnection.js');
+    var gameConf = require('../../game/gameConf.js');
+    var ObjectID = require('mongodb').ObjectID;
 
-router.get('/', function (req, res, next) {
-  mongoConnection(function (db) {
-    db.collection('gameconfs').find({}).sort({_id: 1}).toArray(function (err, result) {
-      if (err) return next(err);
-      res.json(result);
-    });
-  });
-});
-router.get('/run', function(req, res, next) {
-  mongoConnection(function (db) {
-    db.collection('gameconfs').find({role:'run'}).toArray(function (err, result) {
-      if (err) return next(err);
-      res.json(result[0]);
-    });
-  });
-});
+    /* GET /gameConf listing. */
 
-/* POST /gameConf */
-router.post('/', function (req, res, next) {
-  mongoConnection(function (db) {
-    db.collection('gameconfs').insertOne(req.body, function (err, result) {
-      if (err) return next(err);
-      res.json(result);
-    });
-  });
-});
-
-/* PUT /gameConf/:id */
-
-router.put('/:id', function (req, res, next) {
-  mongoConnection(function (db) {
-    db.collection('gameconfs').updateOne({"_id": ObjectID(req.params.id)}, req.body, function (err, result) {
-        if (err) return next(err);
-        gameConf.syncFromDb(function() {
-            gameConf.languageChange();
+    router.get('/', function (req, res, next) {
+        mongoConnection(function (db) {
+            db.collection('gameconfs').find({}).sort({_id: 1}).toArray(function (err, result) {
+                if (err) {
+                    return next(err);
+                }
+                res.json(result);
+            });
         });
-        res.json(result);
-    })
-  });
-});
+    });
+    router.get('/run', function(req, res, next) {
+        mongoConnection(function (db) {
+            db.collection('gameconfs').find({role:'run'}).toArray(function (err, result) {
+                if (err) {
+                    return next(err);
+                }
+                res.json(result[0]);
+            });
+        });
+    });
 
+    /* POST /gameConf */
+    router.post('/', function (req, res, next) {
+        mongoConnection(function (db) {
+            db.collection('gameconfs').insertOne(req.body, function (err, result) {
+                if (err) {
+                    return next(err);
+                }
+                res.json(result);
+            });
+        });
+    });
 
-module.exports = router;
+    /* PUT /gameConf/:id */
+
+    router.put('/:id', function (req, res, next) {
+        mongoConnection(function (db) {
+            db.collection('gameconfs').updateOne({ _id: new ObjectID(req.params.id) }, req.body, function (err, result) {
+                if (err) {
+                    return next(err);
+                }
+                gameConf.syncFromDb(function() {
+                    gameConf.languageChange();
+                });
+                res.json(result);
+            });
+        });
+    });
+
+    module.exports = router;
+
+})();
