b9a78df797ff01a039d1e4b81dca77e5e45f2acf
Mirko Dietrich <mirko.dietrich@l4m1.de>
2015-05-03 19:55:01 +0200

    jshinted homevisit_components/stringFormat.js

--- homevisit_components/stringFormat.js
+++ homevisit_components/stringFormat.js
@@ -1,9 +1,14 @@
-var sprintf = require('sprintf-js').sprintf;
+(function() {
+    'use strict';
 
-if (!String.prototype.format) {
-    String.prototype.format = function() {
-        var args = Array.prototype.slice.call(arguments);
-        args.unshift(String(this));
-        return sprintf.apply(undefined, args);
-    };
-}
+    var sprintf = require('sprintf-js').sprintf;
+
+    if (!String.prototype.format) {
+        String.prototype.format = function() {
+            var args = Array.prototype.slice.call(arguments);
+            args.unshift(String(this));
+            return sprintf.apply(undefined, args);
+        };
+    }
+
+})();
