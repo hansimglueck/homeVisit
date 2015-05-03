b464fc429090130cdf9afbd8e4a97cf2ebc4afc3
Mirko Dietrich <mirko.dietrich@l4m1.de>
2015-05-03 17:10:05 +0200

    jshinted game/clock.js

--- game/clock.js
+++ game/clock.js
@@ -1,28 +1,33 @@
-function GameClock() {
-    this.startTime = null;
-}
+(function() {
+    'use strict';
 
-GameClock.prototype = {
-
-    reset: function() {
-        console.log('gameClock reset');
+    function GameClock() {
         this.startTime = null;
-    },
-
-    start: function() {
-        console.log('gameClock started');
-        this.startTime = new Date().getTime();
-    },
-
-    getCurrentSeconds: function() {
-        if (this.startTime === null) {
-            return 0;
-        }
-        return Math.floor((new Date().getTime() - this.startTime) / 1000);
     }
 
-};
+    GameClock.prototype = {
 
-var gameClock = new GameClock();
+        reset: function() {
+            console.log('gameClock reset');
+            this.startTime = null;
+        },
 
-module.exports = gameClock;
+        start: function() {
+            console.log('gameClock started');
+            this.startTime = new Date().getTime();
+        },
+
+        getCurrentSeconds: function() {
+            if (this.startTime === null) {
+                return 0;
+            }
+            return Math.floor((new Date().getTime() - this.startTime) / 1000);
+        }
+
+    };
+
+    var gameClock = new GameClock();
+
+    module.exports = gameClock;
+
+})();
