9b319f684c13b603e7ec45c3aa5d866e552b286d
Mirko Dietrich <mirko.dietrich@l4m1.de>
2015-05-03 17:29:31 +0200

    jshinted game/items/includes/showAssholes.js

--- game/items/includes/showAssholes.js
+++ game/items/includes/showAssholes.js
@@ -1,28 +1,37 @@
-var playerManager = require('../../playerManager.js');
+(function() {
+    'use strict';
 
-module.exports = {
-    executeItem: function () {
-        this.data = [];
-        var self = this;
-        console.log(JSON.stringify(playerManager.gameEvents));
-        playerManager.players.forEach(function (player) {
-            var negs = [];
-            playerManager.players.forEach(function(player){
-                negs[player.playerId] = 0;
-            });
-            playerManager.gameEvents.forEach(function(event){
-                if (event.playerId == player.playerId && event.event.type === "score" && event.event.reason === "rating" && event.event.value == "-1") negs[event.event.otherPlayerId] += 1;
-            });
-            self.data[player.playerId] = negs;
-        });
-        this.mapToDevice();
-    },
-    getWsContent: function () {
-        return {
-            type: this.type,
-            data: this.data,
-            silent: this.silent
-        };
-    }
-};
+    var playerManager = require('../../playerManager.js');
 
+    module.exports = {
+        executeItem: function () {
+            this.data = [];
+            var self = this;
+            console.log(JSON.stringify(playerManager.gameEvents));
+            playerManager.players.forEach(function (player) {
+                var negs = [];
+                playerManager.players.forEach(function(player){
+                    negs[player.playerId] = 0;
+                });
+                playerManager.gameEvents.forEach(function(event){
+                    if (event.playerId === player.playerId &&
+                        event.event.type === "score" &&
+                        event.event.reason === "rating" &&
+                        event.event.value === "-1") {
+                        negs[event.event.otherPlayerId] += 1;
+                    }
+                });
+                self.data[player.playerId] = negs;
+            });
+            this.mapToDevice();
+        },
+        getWsContent: function () {
+            return {
+                type: this.type,
+                data: this.data,
+                silent: this.silent
+            };
+        }
+    };
+
+})();
