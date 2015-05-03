09b2ba195f921d4222952e465d7e28805aa911f0
Mirko Dietrich <mirko.dietrich@l4m1.de>
2015-05-03 17:13:53 +0200

    jshinted game/items/includes/deal.js

--- game/items/includes/deal.js
+++ game/items/includes/deal.js
@@ -1,30 +1,36 @@
-var playerManager = require('../../playerManager.js');
+(function() {
+    'use strict';
 
-module.exports = {
-    executeItem: function () {
-        this.mapToDevice();
-    },
-    getWsContent: function () {
-        var lang = require('../../gameConf').conf.language;
-        return {
-            type: this.type,
-            text: this.text[lang],
-            silent: this.silent,
-            dealType: this.dealType,
-            maxSteps: this.maxSteps
-        };
-    },
-    finishItem: function() {
-        Object.keys(playerManager.deals).forEach(function (key) {
-            var deal = playerManager.deals[key];
-            if (deal.state < 3) deal.state = 4;
-            playerManager.sendMessage(deal.player0Id, "deal", deal);
-            playerManager.sendMessage(deal.player1Id, "deal", deal);
-            playerManager.players[deal.player0Id].busy = false;
-            playerManager.players[deal.player1Id].busy = false;
-            playerManager.sendPlayerStatus(-1);
-        });
+    var playerManager = require('../../playerManager.js');
 
-    }
-};
+    module.exports = {
+        executeItem: function () {
+            this.mapToDevice();
+        },
+        getWsContent: function () {
+            var lang = require('../../gameConf').conf.language;
+            return {
+                type: this.type,
+                text: this.text[lang],
+                silent: this.silent,
+                dealType: this.dealType,
+                maxSteps: this.maxSteps
+            };
+        },
+        finishItem: function() {
+            Object.keys(playerManager.deals).forEach(function (key) {
+                var deal = playerManager.deals[key];
+                if (deal.state < 3) {
+                    deal.state = 4;
+                }
+                playerManager.sendMessage(deal.player0Id, "deal", deal);
+                playerManager.sendMessage(deal.player1Id, "deal", deal);
+                playerManager.players[deal.player0Id].busy = false;
+                playerManager.players[deal.player1Id].busy = false;
+                playerManager.sendPlayerStatus(-1);
+            });
 
+        }
+    };
+
+})();
