2e506ab75a7131515d892896703abe7f8d730b7f
Mirko Dietrich <mirko.dietrich@l4m1.de>
2015-05-03 17:23:55 +0200

    jshinted game/items/includes/rating.js

--- game/items/includes/rating.js
+++ game/items/includes/rating.js
@@ -1,29 +1,33 @@
-var playerManager = require('../../playerManager.js');
+(function() {
+    'use strict';
 
-module.exports = {
-    executeItem: function () {
-        this.mapToDevice();
-    },
-    getWsContent: function() {
-        var bestWorst;
-        var bestWorstArr;
-        if (this.ratingType == "oneTeam") {
-            bestWorstArr = playerManager.getPlayerGroup(this.bestWorst);
-            if (bestWorstArr.length > 0) bestWorst = bestWorstArr.map(function (x) {
-                return x.playerId
-            });
+    var playerManager = require('../../playerManager.js');
+
+    module.exports = {
+        executeItem: function () {
+            this.mapToDevice();
+        },
+        getWsContent: function() {
+            var bestWorst;
+            var bestWorstArr;
+            if (this.ratingType === "oneTeam") {
+                bestWorstArr = playerManager.getPlayerGroup(this.bestWorst);
+                if (bestWorstArr.length > 0) {
+                    bestWorst = bestWorstArr.map(function (x) {
+                        return x.playerId;
+                    });
+                }
+            }
+            var lang = require('../../gameConf').conf.language;
+            return {
+                type: this.type,
+                ratingType: this.ratingType,
+                posNeg: this.posNeg,
+                playerId: bestWorst,
+                silent: this.silent,
+                text: this.text[lang]
+            };
         }
-        var lang = require('../../gameConf').conf.language;
-        return {
-            type: this.type,
-            ratingType: this.ratingType,
-            posNeg: this.posNeg,
-            playerId: bestWorst,
-            silent: this.silent,
-            text: this.text[lang]
-        };
-    }
-};
+    };
 
-
-
+})();
