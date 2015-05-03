eb68fc22cd9de671d87edededd8fbc48bcaeb8e7
Mirko Dietrich <mirko.dietrich@l4m1.de>
2015-05-03 17:31:33 +0200

    jshinted game/items/includes/vote.js

--- game/items/includes/vote.js
+++ game/items/includes/vote.js
@@ -1,59 +1,56 @@
-var OptionPoll = require('../../polls/OptionPoll.js');
-var NumberPoll = require('../../polls/NumberPoll.js');
-var playerManager = require('../../playerManager.js');
+(function() {
+    'use strict';
 
-module.exports = {
-    test: "test",
+    var OptionPoll = require('../../polls/OptionPoll.js');
+    var NumberPoll = require('../../polls/NumberPoll.js');
+    var playerManager = require('../../playerManager.js');
 
-    executeItem: function () {
-        var poll;
-        switch (this.voteType) {
-            case "customOptions":
-            case "customMultipleOptions":
-                this.voteOptions.forEach(function (opt, id) {
-                    opt.value = id;
+    module.exports = {
+        executeItem: function () {
+            var poll;
+            switch (this.voteType) {
+                case "customOptions":
+                case "customMultipleOptions":
+                    this.voteOptions.forEach(function (opt, id) {
+                        opt.value = id;
+                    });
+                    poll = new OptionPoll(this);
+                    break;
+                case "playerChoice":
+                    this.voteOptions = playerManager.getPlayerGroup("joined").map(function(player){
+                        return {value: player.playerId, text: player.playerId};
+                    });
+                    poll = new OptionPoll(this);
+                    break;
+                case "enterNumber":
+                    poll = new NumberPoll(this);
+                    break;
+                default:
+                    break;
+            }
+            if (typeof poll !== 'undefined') {
+                poll.onFinish(this, function (result) {
+                    this.step(result);
                 });
-                poll = new OptionPoll(this);
-                break;
-            case "playerChoice":
-                this.voteOptions = playerManager.getPlayerGroup("joined").map(function(player){
-                    return {value: player.playerId, text: player.playerId};
-                });
-                poll = new OptionPoll(this);
-                break;
-            case "countryChoice":
-                var lang = this.language;
-                this.voteOptions = data.getEUcountries().map(function (c) {
-                    return {value: c.id, text: c[lang]}
-                });
-                poll = new OptionPoll(this);
-                break;
+                this.poll = poll;
+                //this.polls ist "static"
+                this.polls[this.poll.id] = this.poll;
 
-            case "enterNumber":
-                poll = new NumberPoll(this);
-                break;
-            default:
-                break;
+                this.mapToDevice();
+            }
+            else {
+                console.log('Warning: Vote without voteOptions!');
+            }
+        },
+        getData: function () {
+            if (typeof this.poll !== "undefined") {
+                return this.poll.getResult();
+            }
+            return null;
+        },
+        getWsContent: function () {
+            return this.poll.getPollWsContent();
         }
-        if (typeof poll !== 'undefined') {
-            poll.onFinish(this, function (result) {
-                this.step(result);
-            });
-            this.poll = poll;
-            //this.polls ist "static"
-            this.polls[this.poll.id] = (this.poll);
+    };
 
-            this.mapToDevice();
-        }
-        else {
-            console.log('Warning: Vote without voteOptions!');
-        }
-    },
-    getData: function () {
-        if (typeof this.poll !== "undefined") return this.poll.getResult();
-        else return null;
-    },
-    getWsContent: function () {
-        return this.poll.getPollWsContent();
-    }
-};
+})();
