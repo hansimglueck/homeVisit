7d75c2ca189faf3d89f017cfbcd7659eb29d5820
Mirko Dietrich <mirko.dietrich@l4m1.de>
2015-05-03 23:07:56 +0200

    jshinted mc/js/app.js

--- mc/js/app.js
+++ mc/js/app.js
@@ -1,77 +1,72 @@
-'use strict';
+(function() {
+    'use strict';
 
-/**
- * @ngdoc overview
- * @name homeVisitMCApp
- * @description
- * # homeVisitMCApp
- *
- * Main module of the application.
- */
-angular
-    .module('homeVisitMCApp', [
-        'ngAnimate',
-        'ngResource',
-        'ngRoute',
-        'WebsocketServices',
-        'mcAppServices',
-        'hvPlayerColors',
-        'playerDirectives',
-        'mcDirectives',
-        'hvSetFactory',
-        'hvItemOptions',
-        'hvLanguage',
-        'angularModalService',
-        'gettext'
-    ])
-    .config(function ($routeProvider) {
-        $routeProvider
-            .when('/', {
-                redirectTo: '/polls'
-            })
-            .when('/polls', {
-                templateUrl: '/mc/views/dummies.html',
-                controller: 'DummiesCtrl'
-            })
-            .when('/players', {
-                templateUrl: '/mc/views/players.html',
-                controller: 'PlayersCtrl'
-            })
-            .when('/players/playback', {
-                templateUrl: '/mc/views/players.html',
-                controller: 'PlayersCtrl'
-            })
-            .when('/players/:playerId', {
-                templateUrl: '/mc/views/players.html',
-                controller: 'PlayersCtrl'
-            })
-            .when('/matching', {
-                templateUrl: '/mc/views/matching.html',
-                controller: 'MatchingCtrl'
-            })
-            .when('/deck', {
-                templateUrl: '/mc/views/deck.html',
-                controller: 'DeckCtrl'
-            })
-            .when('/about', {
-                templateUrl: '/mc/views/about.html',
-                controller: 'MatchingCtrl'
-            })
-            .when('/maintenance', {
-                templateUrl: '/mc/views/maintenance.html',
-                controller: 'PlaybackCtrl'
-            })
-            .otherwise({
-                redirectTo: '/'
+    angular
+        .module('homeVisitMCApp', [
+            'ngAnimate',
+            'ngResource',
+            'ngRoute',
+            'WebsocketServices',
+            'mcAppServices',
+            'hvPlayerColors',
+            'playerDirectives',
+            'mcDirectives',
+            'hvSetFactory',
+            'hvItemOptions',
+            'hvLanguage',
+            'angularModalService',
+            'gettext'
+        ])
+        .config(function ($routeProvider) {
+            $routeProvider
+                .when('/', {
+                    redirectTo: '/polls'
+                })
+                .when('/polls', {
+                    templateUrl: '/mc/views/dummies.html',
+                    controller: 'DummiesCtrl'
+                })
+                .when('/players', {
+                    templateUrl: '/mc/views/players.html',
+                    controller: 'PlayersCtrl'
+                })
+                .when('/players/playback', {
+                    templateUrl: '/mc/views/players.html',
+                    controller: 'PlayersCtrl'
+                })
+                .when('/players/:playerId', {
+                    templateUrl: '/mc/views/players.html',
+                    controller: 'PlayersCtrl'
+                })
+                .when('/matching', {
+                    templateUrl: '/mc/views/matching.html',
+                    controller: 'MatchingCtrl'
+                })
+                .when('/deck', {
+                    templateUrl: '/mc/views/deck.html',
+                    controller: 'DeckCtrl'
+                })
+                .when('/about', {
+                    templateUrl: '/mc/views/about.html',
+                    controller: 'MatchingCtrl'
+                })
+                .when('/maintenance', {
+                    templateUrl: '/mc/views/maintenance.html',
+                    controller: 'PlaybackCtrl'
+                })
+                .otherwise({
+                    redirectTo: '/'
+                });
+        })
+        .run(function (Socket, Status, Deck, gettextCatalog) {
+            Socket.connect('MC', function() {
+                Socket.emit('getLanguage');
             });
-    })
-    .run(function (Socket, Status, Deck, gettextCatalog) {
-        Socket.connect('MC', function() {
-            Socket.emit('getLanguage');
+            Status.start();
+            Deck.start();
+            Socket.on('languageChange', function (data) {
+                gettextCatalog.setCurrentLanguage(data.language);
+            });
         });
-        Status.start();
-        Deck.start();
-        Socket.on('languageChange', function (data) {
-            gettextCatalog.setCurrentLanguage(data.language);
-        });
-    });
+
+})();
