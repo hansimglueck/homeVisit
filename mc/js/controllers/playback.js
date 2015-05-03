a23a99658802ffdb7e9653379913c01eebfde628
Mirko Dietrich <mirko.dietrich@l4m1.de>
2015-05-03 23:18:49 +0200

    jshinted mc/js/controllers/playback.js

--- mc/js/controllers/playback.js
+++ mc/js/controllers/playback.js
@@ -1,31 +1,36 @@
-angular.module('homeVisitMCApp')
-    .controller('PlaybackCtrl', function ($scope, Playback, Deck) {
-        $scope.playback = Playback.playback;
-        $scope.alert = Playback.alert;
-        $scope.clock = function() {
-            var secs = Deck.clockSeconds, minutes, hours;
+(function() {
+    'use strict';
 
-            // test values here:
-            // secs = 0; // 0:10h
-            // secs = 59; // 0:00h
-            // secs = 60; // 0:01h
-            // secs = 10 * 60; // 0:10h
-            // secs = 60 * 60; // 1:00h
-            // secs = 61 * 60; // 1:01h
-            // secs = 119 * 60; // 1:59h
-            // secs = 120 * 60; // 2:00h
-            // secs = 121 * 60; // 2:01h
+    angular.module('homeVisitMCApp')
+        .controller('PlaybackCtrl', function ($scope, Playback, Deck) {
+            $scope.playback = Playback.playback;
+            $scope.alert = Playback.alert;
+            $scope.clock = function() {
+                var secs = Deck.clockSeconds, minutes, hours;
 
-            if (typeof secs !== 'number') {
-                minutes = 0;
-                hours = 0;
-            }
-            else {
-                minutes = Math.floor((secs / 60) % 60);
-                hours = Math.floor((secs / 60 - minutes) / 60);
-            }
-            return String(hours) + ':'
-                + (String(minutes).length < 2 ? '0' : '')
-                + String(minutes);
-        };
-    });
+                // test values here:
+                // secs = 0; // 0:10h
+                // secs = 59; // 0:00h
+                // secs = 60; // 0:01h
+                // secs = 10 * 60; // 0:10h
+                // secs = 60 * 60; // 1:00h
+                // secs = 61 * 60; // 1:01h
+                // secs = 119 * 60; // 1:59h
+                // secs = 120 * 60; // 2:00h
+                // secs = 121 * 60; // 2:01h
+
+                if (typeof secs !== 'number') {
+                    minutes = 0;
+                    hours = 0;
+                }
+                else {
+                    minutes = Math.floor(secs / 60 % 60);
+                    hours = Math.floor((secs / 60 - minutes) / 60);
+                }
+                return String(hours) + ':' +
+                    (String(minutes).length < 2 ? '0' : '') +
+                    String(minutes);
+            };
+        });
+
+})();
