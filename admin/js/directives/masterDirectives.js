890cb9be37a9f8f2cf0b59a2aa4de4127855698c
Mirko Dietrich <mirko.dietrich@l4m1.de>
2015-05-03 23:03:51 +0200

    jshinted admin/js/directives/masterDirectives.js

--- admin/js/directives/masterDirectives.js
+++ admin/js/directives/masterDirectives.js
@@ -1,57 +1,58 @@
-/**
- * Created by jeanbluer on 06.02.15.
- */
+(function() {
+    'use strict';
 
-var baseUrl = '/admin/views';
+    var baseUrl = '/admin/views';
 
-angular.module('masterDirectives', [])
+    angular.module('masterDirectives', [])
 
-    .directive('showDeviceList', function() {
-        return {
-            restrict: 'AE',
-            replace: 'true',
-            templateUrl: baseUrl + '/show-device-list.html'
-        };
-    })
-    .directive('masterButtons', function() {
-        return {
-            restrict: 'AE',
-            replace: 'true',
-            templateUrl: baseUrl + '/master-buttons.html'
-        };
-    })
-    .directive('logDisplay', function() {
-        return {
-            restrict: 'AE',
-            replace: 'true',
-            templateUrl: baseUrl + '/log-display.html'
-        };
-    })
-    .directive('masterContent', function() {
-        return {
-            restrict: 'AE',
-            replace: 'true',
-            templateUrl: baseUrl + '/master-content.html'
-        };
-    })
-    .directive('gameConf', function() {
-        return {
-            restrict: 'AE',
-            replace: 'true',
-            templateUrl: baseUrl + '/game-conf.html'
-        };
-    })
-    .directive('raspiManager', function() {
-        return {
-            restrict: 'AE',
-            replace: 'true',
-            templateUrl: baseUrl + '/raspi-manager.html'
-        };
-    })
-    .directive('playerList', function() {
-        return {
-            restrict: 'AE',
-            replace: 'true',
-            templateUrl: baseUrl + '/player-list.html'
-        };
-    });
+        .directive('showDeviceList', function() {
+            return {
+                restrict: 'AE',
+                replace: 'true',
+                templateUrl: baseUrl + '/show-device-list.html'
+            };
+        })
+        .directive('masterButtons', function() {
+            return {
+                restrict: 'AE',
+                replace: 'true',
+                templateUrl: baseUrl + '/master-buttons.html'
+            };
+        })
+        .directive('logDisplay', function() {
+            return {
+                restrict: 'AE',
+                replace: 'true',
+                templateUrl: baseUrl + '/log-display.html'
+            };
+        })
+        .directive('masterContent', function() {
+            return {
+                restrict: 'AE',
+                replace: 'true',
+                templateUrl: baseUrl + '/master-content.html'
+            };
+        })
+        .directive('gameConf', function() {
+            return {
+                restrict: 'AE',
+                replace: 'true',
+                templateUrl: baseUrl + '/game-conf.html'
+            };
+        })
+        .directive('raspiManager', function() {
+            return {
+                restrict: 'AE',
+                replace: 'true',
+                templateUrl: baseUrl + '/raspi-manager.html'
+            };
+        })
+        .directive('playerList', function() {
+            return {
+                restrict: 'AE',
+                replace: 'true',
+                templateUrl: baseUrl + '/player-list.html'
+            };
+        });
+
+})();
