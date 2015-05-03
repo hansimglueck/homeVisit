c046ba7cb73b86132b5351cfe0a1ae63c82784da
Mirko Dietrich <mirko.dietrich@l4m1.de>
2015-05-03 17:21:41 +0200

    jshinted game/items/includes/inlineSwitch.js

--- game/items/includes/inlineSwitch.js
+++ game/items/includes/inlineSwitch.js
@@ -1,26 +1,37 @@
-module.exports = {
-    executeItem: function () {
-        var option = this.param;
-        if (this.inlineSwitchSource === 'previousStep') {
-            var data = this.previous.getData();
-            if (data !== null) option = data.complete ? data.voteOptions[0].value : -1;
-        }
-        this.log("looking for deck for option " + option);
-        if (typeof this.inlineDecks === "undefined") return;
-        var deck = this.inlineDecks[option];
-        if (typeof deck == "undefined") {
-            this.log("no matching option found", true);
-            this.step();
-        }
-        else {
-            this.log("inserting deck for option " + option, true);
-            var oldNext = this.next;
-            this.next = null;
-            for (var i = 0; i < deck.items.length; i++) {
-                this.appendItem(new SequenceItem(null, deck.items[i], this.index + ":" + option + ":" + i, true));
+(function() {
+    'use strict';
+
+    var SequenceItem = require('../SequenceItem');
+
+    module.exports = {
+        executeItem: function () {
+            var option = this.param;
+            if (this.inlineSwitchSource === 'previousStep') {
+                var data = this.previous.getData();
+                if (data !== null) {
+                    option = data.complete ? data.voteOptions[0].value : -1;
+                }
             }
-            this.appendItem(oldNext);
-            this.step();
+            this.log("looking for deck for option " + option);
+            if (typeof this.inlineDecks === "undefined") {
+                return;
+            }
+            var deck = this.inlineDecks[option];
+            if (typeof deck === "undefined") {
+                this.log("no matching option found", true);
+                this.step();
+            }
+            else {
+                this.log("inserting deck for option " + option, true);
+                var oldNext = this.next;
+                this.next = null;
+                for (var i = 0; i < deck.items.length; i++) {
+                    this.appendItem(new SequenceItem(null, deck.items[i], this.index + ":" + option + ":" + i, true));
+                }
+                this.appendItem(oldNext);
+                this.step();
+            }
         }
-    }
-};
+    };
+
+})();
