ddad72bbff76444ed4d8daf487a5b68f88ec4afd
Mirko Dietrich <mirko.dietrich@l4m1.de>
2015-05-03 17:16:17 +0200

    jshinted game/items/includes/default.js

--- game/items/includes/default.js
+++ game/items/includes/default.js
@@ -1,13 +1,18 @@
-module.exports = {
-    executeItem: function () {
-        this.mapToDevice();
-    },
-    getWsContent: function() {
-        var lang = require('../../gameConf').conf.language;
-        return {
-            type: this.type,
-            text: this.text[lang],
-            silent: this.silent
-        };
-    }
-};
+(function() {
+    'use strict';
+
+    module.exports = {
+        executeItem: function () {
+            this.mapToDevice();
+        },
+        getWsContent: function() {
+            var lang = require('../../gameConf').conf.language;
+            return {
+                type: this.type,
+                text: this.text[lang],
+                silent: this.silent
+            };
+        }
+    };
+
+})();
