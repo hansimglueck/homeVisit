b73fa8ca5fc627143b31ae92518e4de6b5a25697
Mirko Dietrich <mirko.dietrich@l4m1.de>
2015-05-03 17:13:09 +0200

    jshinted game/items/includes/cmd.js

--- game/items/includes/cmd.js
+++ game/items/includes/cmd.js
@@ -1,16 +1,20 @@
-module.exports = {
-    executeItem: function () {
-        this.mapToDevice();
-    },
-    getWsContent: function () {
-        var lang = require('../../gameConf').conf.language;
-        return {
-            type: this.type,
-            command: this.text[lang],
-            silent: this.silent,
-            param: this.parameter,
-            device: this.device
-        };
-    }
-};
+(function() {
+    'use strict';
 
+    module.exports = {
+        executeItem: function () {
+            this.mapToDevice();
+        },
+        getWsContent: function () {
+            var lang = require('../../gameConf').conf.language;
+            return {
+                type: this.type,
+                command: this.text[lang],
+                silent: this.silent,
+                param: this.parameter,
+                device: this.device
+            };
+        }
+    };
+
+})();
