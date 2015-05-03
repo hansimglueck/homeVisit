e628ff5558df01485ba9fd24228ee197a50a35cd
Mirko Dietrich <mirko.dietrich@l4m1.de>
2015-05-03 19:52:07 +0200

    jshinted server/routes/main.js

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,41 +1,45 @@
-var router = require('express').Router();
-var game = require('../../game/game.js');
-var playerManager = require('../../game/playerManager.js');
+(function() {
+    'use strict';
 
-router.get('/', function(req, res, next) {
-    res.redirect('/player');
-});
+    var router = require('express').Router();
+    var game = require('../../game/game.js');
 
-router.get('/admin', function(req, res, next) {
-  res.sendFile('index.html', { root: __dirname + '/../../admin' });
-});
-
-router.get('/player', function(req, res, next) {
-  res.sendFile('index.html', { root: __dirname + '/../../player' });
-});
-
-router.get('/mc', function(req, res, next) {
-    res.sendFile('mc/index.html', { root: __dirname +"/../public"  });
-});
-
-router.get('/log', function(req, res, next) {
-    var results = [];
-    Object.keys(game.sequence.polls).forEach(function(poll){
-        var res = null;
-        try {
-            res = game.sequence.polls[poll].getResult();
-        }
-        catch (e) {};
-        results.push(res);
+    router.get('/', function(req, res, next) {
+        res.redirect('/player');
     });
-    console.log(game.sequence);
-    var sequence = game.sequence.getExecuteTime();
-    console.log("seq:"+sequence);
-    res.json({sequence: sequence, results: results});
-});
 
-router.get('/emulator', function(req, res, next) {
-    res.render('emulator');
-});
+    router.get('/admin', function(req, res, next) {
+        res.sendFile('index.html', { root: __dirname + '/../../admin' });
+    });
 
-module.exports = router;
+    router.get('/player', function(req, res, next) {
+        res.sendFile('index.html', { root: __dirname + '/../../player' });
+    });
+
+    router.get('/mc', function(req, res, next) {
+        res.sendFile('mc/index.html', { root: __dirname +"/../public"  });
+    });
+
+    router.get('/log', function(req, res, next) {
+        var results = [];
+        Object.keys(game.sequence.polls).forEach(function(poll){
+            var res = null;
+            try {
+                res = game.sequence.polls[poll].getResult();
+            }
+            catch (e) {}
+            results.push(res);
+        });
+        console.log(game.sequence);
+        var sequence = game.sequence.getExecuteTime();
+        console.log("seq:"+sequence);
+        res.json({sequence: sequence, results: results});
+    });
+
+    router.get('/emulator', function(req, res, next) {
+        res.render('emulator');
+    });
+
+    module.exports = router;
+
+})();
