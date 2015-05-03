b90b72f4a3e0a8adcc97dd44fb19ddaab58c315e
Mirko Dietrich <mirko.dietrich@l4m1.de>
2015-05-03 17:18:29 +0200

    jshinted game/items/includes/eval.js

--- game/items/includes/eval.js
+++ game/items/includes/eval.js
@@ -1,12 +1,17 @@
-module.exports = {
-    executeItem: function () {
-        console.log(this.eval);
-        var lang = require('../../gameConf').conf.language;
-        try {
-            eval(this.text[lang]);
-        } catch (e) {
-            console.log(e.stack);
-        }
-    }
-};
+(function() {
+    'use strict';
 
+    module.exports = {
+        executeItem: function () {
+            /* jshint evil: true */
+            console.log(this.eval);
+            var lang = require('../../gameConf').conf.language;
+            try {
+                eval(this.text[lang]);
+            } catch (e) {
+                console.log(e.stack);
+            }
+        }
+    };
+
+})();
