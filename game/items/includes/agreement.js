da17245457f192811e0c5ca16a2fe0e348acf23b
Mirko Dietrich <mirko.dietrich@l4m1.de>
2015-05-03 17:09:48 +0200

    jshinted game/items/includes/agreement.js

--- game/items/includes/agreement.js
+++ game/items/includes/agreement.js
@@ -1,35 +1,41 @@
-var playerManager = require('../../playerManager.js');
-var Agreement = require('../../polls/Agreement.js');
+(function() {
+    'use strict';
 
+    var playerManager = require('../../playerManager.js');
+    var Agreement = require('../../polls/Agreement.js');
 
-module.exports = {
-    executeItem: function () {
-        this.playerIds = playerManager.getPlayerGroup(this.agreementOption).map(function (player) {
-            return player.playerId;
-        });
-        var self = this;
-        var poll;
-        if (this.type === "agreement") {
-            poll = new Agreement(this);
-            poll.onFinish(playerManager, function (result) {
-                if (result.fullfilled) playerManager.setAgreement(poll);
-                poll.playerIds.forEach(function (pid) {
-                    playerManager.sendMessage(pid, "display", {type: "card", text: result.text});
-                })
-            })
+    module.exports = {
+        executeItem: function () {
+            this.playerIds = playerManager.getPlayerGroup(this.agreementOption).map(function (player) {
+                return player.playerId;
+            });
+            var poll;
+            if (this.type === "agreement") {
+                poll = new Agreement(this);
+                poll.onFinish(playerManager, function (result) {
+                    if (result.fullfilled) {
+                        playerManager.setAgreement(poll);
+                    }
+                    poll.playerIds.forEach(function (pid) {
+                        playerManager.sendMessage(pid, "display", {type: "card", text: result.text});
+                    });
+                });
+            }
+            this.poll = poll;
+
+            this.polls[this.poll.id] = this.poll;
+
+            this.mapToDevice();
+        },
+        getData: function() {
+            if (typeof this.poll !== "undefined") {
+                return this.poll.getResult();
+            }
+            return null;
+        },
+        getWsContent: function () {
+            return this.poll.getPollWsContent();
         }
-        this.poll = poll;
+    };
 
-        this.polls[this.poll.id] = (this.poll);
-
-        this.mapToDevice();
-    },
-    getData: function() {
-        if (typeof this.poll !== "undefined") return this.poll.getResult();
-        else return null;
-    },
-    getWsContent: function () {
-        return this.poll.getPollWsContent();
-    }
-};
-
+})();
