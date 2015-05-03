cd55670eb01b28ac5b73323b24ab0b03b6270f72
Mirko Dietrich <mirko.dietrich@l4m1.de>
2015-05-03 17:10:42 +0200

    jshinted game/gettext.js

--- game/gettext.js
+++ game/gettext.js
@@ -1,20 +1,25 @@
-var fs = require('fs');
-var Gettext = require('node-gettext');
+(function() {
+    'use strict';
 
-var gt = new Gettext();
-var poPath = __dirname + '/../po/';
+    var fs = require('fs');
+    var Gettext = require('node-gettext');
 
-['en', 'de', 'cs', 'da', 'fr', 'nl', 'no', 'pl', 'pt'].forEach(function(langCode) {
-    gt.addTextdomain(langCode, fs.readFileSync(poPath + langCode + '.po'));
-});
+    var gt = new Gettext();
+    var poPath = __dirname + '/../po/';
 
-module.exports = {
-    textdomain: function() {
-        return gt.textdomain.apply(gt, arguments);
-    },
-    gettext: function() {
-        var gc = require('./gameConf');
-        gt.textdomain(gc.conf.language);
-        return gt.gettext.apply(gt, arguments);
-    }
-};
+    ['en', 'de', 'cs', 'da', 'fr', 'nl', 'no', 'pl', 'pt'].forEach(function(langCode) {
+        gt.addTextdomain(langCode, fs.readFileSync(poPath + langCode + '.po'));
+    });
+
+    module.exports = {
+        textdomain: function() {
+            return gt.textdomain.apply(gt, arguments);
+        },
+        gettext: function() {
+            var gc = require('./gameConf');
+            gt.textdomain(gc.conf.language);
+            return gt.gettext.apply(gt, arguments);
+        }
+    };
+
+})();
