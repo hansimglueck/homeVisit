58886cd77e086569258839245e3632dce87f7ed2
Mirko Dietrich <mirko.dietrich@l4m1.de>
2015-05-13 00:14:08 +0200

    KEINE -1 nur weil hans das so wollte...

--- shellscripts/tunnel.sh
+++ shellscripts/tunnel.sh
@@ -6,7 +6,7 @@ if [ -e ~/homeVisit/DIZZICONF ]; then
     # calculate port number from hostname
     if [[ ${HOSTNAME} =~ ^homevisitpi([0-9]+)$ ]]; then
         num=${BASH_REMATCH[1]}
-        port=$((DIZZIPORT + num - 1))
+        port=$((DIZZIPORT + num))
     else
         echo "HOSTNAME is not homevisitpiN!"
         exit -1
