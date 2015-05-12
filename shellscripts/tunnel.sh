5ed8c9bd201a9453440307c480fd6f8b76ef19f9
Mirko Dietrich <mirko.dietrich@l4m1.de>
2015-05-12 23:05:29 +0200

    tunnel script uses hostname for calculating remote port

--- shellscripts/tunnel.sh
+++ shellscripts/tunnel.sh
@@ -2,7 +2,19 @@
 
 if [ -e ~/homeVisit/DIZZICONF ]; then
     source ~/homeVisit/DIZZICONF
-    /usr/bin/autossh -fN -o "ServerAliveInterval=30" -o "ServerAliveCountMax=99999" -R ${DIZZIPORT}:localhost:22 ${DIZZIHOST}
+
+    # calculate port number from hostname
+    if [[ ${HOSTNAME} =~ ^homevisitpi([0-9]+)$ ]]; then
+        num=${BASH_REMATCH[1]}
+        port=$((DIZZIPORT + num - 1))
+    else
+        echo "HOSTNAME is not homevisitpiN!"
+        exit -1
+    fi
+
+    # start tunnel
+    echo Starting tunnel to ${DIZZIHOST} ${port}
+    /usr/bin/autossh -fN -o "ServerAliveInterval=30" -o "ServerAliveCountMax=99999" -R ${port}:localhost:22 ${DIZZIHOST}
 else
     echo "DIZZICONF not found!"
 fi
