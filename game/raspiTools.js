86fcfd4133a2be37743e78c83ac93aaa12c800f3
h*ns <hans@dizzi.biz>
2015-05-16 13:32:05 +0200

    typo

--- game/raspiTools.js
+++ game/raspiTools.js
@@ -188,7 +188,7 @@
         },
         exportRecordingsToDizzi: function () {
             exec("mongoexport -d homeVisit -c recordings | ssh "+conf.dizziSSH+" mongoimport -d homeVisit -c recordings --upsert", function (error, stdout, stderr) {
-                if (error) console.log("Error in importSessions: " + error);
+                if (error) console.log("Error in exportRecordingsToDizzi: " + error);
                 console.log("Exported Recordings to dizzi " + stdout);
             });
 
