bbd287110e9a7104364eb48cd118813f8b1908e0
h*ns <hans@dizzi.biz>
2015-05-22 01:12:09 +0200

    monitoring-system im raspiTools. wird zentral gecheckt: DB, AP, MC, PLAYER

--- game/raspiTools.js
+++ game/raspiTools.js
@@ -9,6 +9,7 @@
     var wsManager = require('./wsManager.js');
     var conf = require('../homevisitConf');
     var logger = require('log4js').getLogger();
+    var ping = require('ping');
 
 
     function RaspiTools() {
@@ -16,6 +17,37 @@
         this.onlineStateArr = [false, false, false];
         this.onlineState = false;
         this.infoSubscribers = [];
+        this.monitoringTasks = [];
+        this.monitoringTable = [
+            {
+                name: 'ap',
+                type: 'state',
+                state: false,
+                necessary: true,
+                hint: "Plug the AP"
+            },
+            {
+                name: 'mc',
+                type: 'count',
+                state: 0,
+                necessary: 1,
+                hint: "Start/Restart the MC-App"
+            },
+            {
+                name: 'db',
+                type: 'state',
+                state: false,
+                necessary: true,
+                hint: "Use Repair-DB in Brain-Control"
+            },
+            {
+                name: 'player',
+                type: 'count',
+                state: 0,
+                necessary: 8,
+                hint: "Turn on 8 Phones, connect them to WIFI and start/restart the homevisit-App"
+            }
+        ]
     }
 
     RaspiTools.prototype = {
@@ -189,11 +221,75 @@
             });
         },
         exportRecordingsToDizzi: function () {
-            exec("sudo -u pi mongoexport -d homeVisit -c recordings | sudo -u pi ssh "+conf.dizziSSH+" mongoimport -d homeVisit -c recordings --upsert", function (error, stdout, stderr) {
+            exec("sudo -u pi mongoexport -d homeVisit -c recordings | sudo -u pi ssh " + conf.dizziSSH + " mongoimport -d homeVisit -c recordings --upsert", function (error, stdout, stderr) {
                 if (error) logger.error("Error in exportRecordingsToDizzi: " + error);
                 logger.info("Exported Recordings to dizzi " + stdout);
             });
 
+        },
+
+        startSetupMonitoring: function () {
+            this.monitoringTasks.push({context: this, interval: 1000, task: this.checkAP, intervalObject: null});
+            var self = this;
+            this.monitoringTasks.forEach(function (task, id) {
+                task.task.call(task.context);
+                if (task.interval !== 0) {
+                    task.intervalObject = setInterval(function () {
+                        task.task.call(task.context);
+                    }, task.interval);
+                }
+            });
+        },
+        checkAP: function () {
+            var self = this;
+            ping.sys.probe('10.0.0.2', function (isAlive) {
+                logger.debug("AP-Ping: " + isAlive);
+                self.changeMonitoringValue('ap', isAlive);
+            })
+        },
+        changeMonitoringValue: function (name, value) {
+            var monitoringItem = null;
+            var arr = this.monitoringTable.filter(function (m) {
+                return m.name === name
+            });
+            if (arr.length > 0) {
+                monitoringItem = arr[0];
+            }
+            if (monitoringItem === null) {
+                return;
+            }
+            var oldState = monitoringItem.state;
+            switch (monitoringItem.type) {
+                case "state":
+                    monitoringItem.state = value;
+                    break;
+                case "count":
+                    monitoringItem.state += value;
+                    break;
+            }
+            if (monitoringItem.state !== oldState) {
+                this.checkSetupMonitoring(true);
+            }
+        },
+        checkSetupMonitoring: function (print) {
+            var monitoringState = this.monitoringTable.map(function (m) {
+                return {name: m.name, fullfilled: m.state >= m.necessary}
+            }).filter(function (m) {
+                return !m.fullfilled;
+            }).length;
+            logger.debug(this.monitoringTable);
+            logger.info("Monitoring found " + monitoringState + " open Necessitys");
+
+            var message = this.monitoringTable.map(function (m) {
+                var ret = m.name.toUpperCase()+ ": ";
+                ret += (m.state >= m.necessary) ? "OK" : "no good! - Try: "+ m.hint;
+                return ret;
+            }).join("\n");
+            logger.warn(message);
+            if (print) {
+                wsManager.msgDevicesByRole("printer", "display", {type: "info", text: message});
+            }
+            return monitoringState;
         }
 
     };
