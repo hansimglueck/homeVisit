7a7e62856f3ff739d5cfd3c56e89b02f148bf542
hans@dizzi.biz <hans@dizzi.biz>
2017-04-13 17:21:08 +0200

    only show realtively new sessions

--- game/gameConf.js
+++ game/gameConf.js
@@ -5,7 +5,7 @@
     var wsManager = require('./wsManager.js');
     var clone = require('clone');
     var logger = require('log4js').getLogger();
-
+    var sessionRestriction = {$or:[{date:{$gt:new Date(Date.now()-7*24*60*60*1000).toISOString()}}, {sessionId:1}]};
 
     function GameConf() {
         this.conf = {};         //fixe Kongiguration wie startDeck, typeMapping
@@ -71,7 +71,7 @@
         gameSessionsRequest: function(clientId, role) {
             var self = this;
             mongoConnection(function (db) {
-                db.collection('sessions').find().toArray(function(err, sessions) {
+                db.collection('sessions').find(sessionRestriction).toArray(function(err, sessions) {
                     if (err !== null) {
                         throw new Error(err.stack);
                     }
