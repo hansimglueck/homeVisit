419338c4526564e1e69628ebe2119103849b1c08
hans@dizzi.biz <hans@dizzi.biz>
2018-03-26 01:19:15 +0300

    using english sound only if there is no local version, always using english command (cherry picked from commit b9c4dfd)

--- game/items/includes/sound.js
+++ game/items/includes/sound.js
@@ -6,9 +6,10 @@
             this.mapToDevice();
         },
         getWsContent: function() {
+            var lang = require('../../gameConf').conf.language;
             return {
                 type: this.type,
-                text: this.text["en"],
+                text: (typeof this.text[lang] === "undefined" || this.text[lang] == "") ? this.text["en"] : this.text[lang],
                 volume: this.volume,
                 silent: this.silent
             };
