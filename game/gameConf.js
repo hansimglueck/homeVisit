43da5d5926727444ca6b96116f93ad243c023ad2
h*ns <hans@dizzi.biz>
2018-09-02 20:43:37 +0200

    showing all the recent sessions in mc-app

--- game/gameConf.js
+++ game/gameConf.js
@@ -6,7 +6,10 @@
     var clone = require('clone');
     var logger = require('log4js').getLogger();
 
-    var sessionRestriction = {$or:[{$and:[{$or:[{city:"Moskau"},{city:"St. Petersburg"},{city:"Yekaterinburg"},{city:/Russia/i}]},{date:{$gt:new Date(Date.now()-7*24*60*60*1000).toISOString()}}]}, {sessionId:1}]};
+    var sessionRestriction = {$or:[
+        {date:{$gt:new Date(Date.now()-7*24*60*60*1000).toISOString()}},
+        {sessionId:1}
+    ]};
 
     function GameConf() {
         this.conf = {};         //fixe Kongiguration wie startDeck, typeMapping
